1ecdb70f74a46ffae35dd4d008afc83f54129166
pabmacias <pabmacias28@gmail.com>
2017-11-26 21:51:50 -0600

    testing

--- public/script.js
+++ public/script.js
@@ -2,6 +2,18 @@ $(document).ready(function(){
 
     var cantidad = 1;
 
+    $.getJSON( "/maaloka/kitchen", { "status" : "valor1", "message" : "valor2", "listaProductos" : "valor3" } )
+        .done(function( data, textStatus, jqXHR ) {
+            if ( console && console.log ) {
+                console.log( "La solicitud se ha completado correctamente." );
+            }
+        })
+        .fail(function( jqXHR, textStatus, errorThrown ) {
+            if ( console && console.log ) {
+                console.log( "Algo ha fallado: " +  textStatus );
+            }
+    });
+
     $(".rayas").click(function(){
         $(".overlay").css("display", "inline");
         $(".overlay").css("z-index", "1");
@@ -16,9 +28,24 @@ $(document).ready(function(){
         $(".productoOfertas").css("display", "inline");
     });
 
+    $("#oferta").click(function(){
+        var imagenOferta = $("#oferta").height();
+        $(".productoOfertasTb").height($("#oferta").height());
+        $(".productoOfertasTb").height($("#oferta").height());
+        $(".imagenOferta").css("display", "none"); 
+        $(".productoOfertasTb").css("display", "inline");
+        var imagenProducto = $("#imagenOfertasTb1").height();
+        $(".nombreOfertasTb").height((imagenOferta - imagenProducto) / 2);
+    });
+
     $(".productoOfertas").click(function(){
         $(".oferta").css("display", "inline"); 
         $(".productoOfertas").css("display", "none");    
+    });
+
+    $(".productoOfertasTb").click(function(){
+        $(".imagenOferta").css("display", "inline");
+        $(".productoOfertasTb").css("display", "none");    
     });
 
     $(".recomendaciones").click(function(){
