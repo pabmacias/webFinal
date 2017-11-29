a4af6c1dac4febbdfbd0ec8a37fd88eba07b350c
pabmacias <pabmacias28@gmail.com>
2017-11-29 12:33:27 -0600

    hola

--- public/producto.js
+++ public/producto.js
@@ -1,3 +1,13 @@
+function agregarCarrito(){
+    console.log(this.dataset.id); //como le mando el id del producto
+    //window.location.replace("producto.html");
+}
+
+function agregarWishlist(){
+    console.log(this.dataset.id);
+
+}
+
 function getParameterByName(name, url) {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, "\\$&");
@@ -8,6 +18,93 @@ function getParameterByName(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-var foo = getParameterByName('idProduct')
+var idP = getParameterByName('idProduct')
 
-console.log(foo);
+//console.log(foo);
+
+$(document).ready(function(){
+  $.post( "/maaloka/getProduct", { "idProduct" : idP }, null, "json" )
+    .done(function( data, textStatus, jqXHR ) {
+        if ( console && console.log ) {
+          console.log(data);
+          console.log(textStatus);
+          console.log(jqXHR);
+            console.log( "La solicitud se ha completado correctamente." );
+
+            var oferta;
+            var imagen;
+            var nombreProducto;
+            var precio;
+            var descripcion;
+            var detalles;
+            var nombreFabricante;
+            var id;
+
+            if($(window).width() >= 300 && $(window).width() <= 481){
+
+            }
+            else if($(window).width() >= 482 && $(window).width() <= 767){
+
+            }
+            else if($(window).width() >= 768 && $(window).width() <= 991){
+
+            }
+            else if($(window).width() >= 992 && $(window).width() <= 1199){
+
+                oferta = data.data[0].sale;
+                imagen = data.data[0].image;
+                nombreProducto = data.data[0].nameProduct;
+                precio = data.data[0].cost;
+                descripcion = data.data[0].description;
+                detalles = "Posee el Rinnegan";
+                nombreFabricante = "Maaloka"
+                id = 1; //id de cada producto
+                $(".zonaProductosMD").append("<div class='bloqueProducto center'><h1 class='descuento'>"+ oferta +"%</h1><img class='width100' src='"+ imagen +"'><div class='row espacio4XS'></div><div class='row'><div class='col-7-10'><h1 class='font200 floatLeft'>"+ nombreProducto +"</h1></div><div class='col-3-10'><h1 class='cantidad font250'>$"+ precio.toFixed(2) +"</h1></div></div><div class='row espacio2XS'></div><h1 class='floatLeft font150'>Descripcion: "+ descripcion +"</h1><div class='row espacio2XS'></div><h1 class='floatLeft font150'>Detalles: "+ detalles +"</h1><div class='row espacio2XS'></div><h1 class='floatLeft font150'>Nombre del fabricante: "+ nombreFabricante +"</h1><div class='row espacio2XS'></div><div class='row inline'><div class='col-1-5'><img class='width100 productoCart' data-id='"+ id +"' src='blueCart.png'></div><div class='col-1-5 floatRight'><img class='width100 productoHeart' data-id='"+ id +"'' src='blueHeart.png'></div></div></div>");
+
+                var listaHeart = document.getElementsByClassName("productoHeart");
+                var listaCart = document.getElementsByClassName("productoCart");
+
+                for(var i in listaHeart){
+                    listaHeart[i].onclick = agregarWishlist;
+                }
+
+                for(var i in listaCart){
+                    listaCart[i].onclick = agregarCarrito;
+                }
+            }
+            else if($(window).width() >= 1200){
+
+              oferta = data.data[0].sale;
+              imagen = data.data[0].image;
+              nombreProducto = data.data[0].nameProduct;
+              precio = data.data[0].cost;
+              descripcion = data.data[0].description;
+              detalles = "Posee el Rinnegan";
+              nombreFabricante = "Maaloka"
+                id = i; //id de cada producto
+                $(".zonaProductosLG").append("<div class='bloqueProducto center'><h1 class='descuento'>"+ oferta +"%</h1><img class='width100' src='"+ imagen +"'><div class='row espacio4XS'></div><div class='row'><div class='col-7-10'><h1 class='font200 floatLeft'>"+ nombreProducto +"</h1></div><div class='col-3-10'><h1 class='cantidad font250'>$"+ precio.toFixed(2) +"</h1></div></div><div class='row espacio2XS'></div><h1 class='floatLeft font150'>Descripcion: "+ descripcion +"</h1><div class='row espacio2XS'></div><h1 class='floatLeft font150'>Detalles: "+ detalles +"</h1><div class='row espacio2XS'></div><h1 class='floatLeft font150'>Nombre del fabricante: "+ nombreFabricante +"</h1><div class='row espacio2XS'></div><div class='row inline'><div class='col-1-5'><img class='width100 productoCart' data-id='"+ id +"' src='blueCart.png'></div><div class='col-1-5 floatRight'><img class='width100 productoHeart' data-id='"+ id +"' src='blueHeart.png'></div></div></div>");
+
+                var listaHeart = document.getElementsByClassName("productoHeart");
+                var listaCart = document.getElementsByClassName("productoCart");
+
+                for(var i in listaHeart){
+                    listaHeart[i].onclick = agregarWishlist;
+                }
+
+                for(var i in listaCart){
+                    listaCart[i].onclick = agregarCarrito;
+                }
+            }
+        }
+    })
+    .fail(function( jqXHR, textStatus, errorThrown ) {
+        if ( console && console.log ) {
+            console.log( "La solicitud a fallado: " +  textStatus);
+        }
+  });
+
+});
+
+$(window).on('resize', function(){
+   location.reload();
+});
